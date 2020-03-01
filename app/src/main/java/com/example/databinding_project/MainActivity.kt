cc588e96b1da69d8ef01e78dc5c18e3781a97eae
adioses <arthur.felipe.dioses.reto@everis.com>
2020-03-01 00:57:42 -0500

    Se agrego databinding al MainActivity

--- app/src/main/java/com/example/databinding_project/MainActivity.kt
+++ app/src/main/java/com/example/databinding_project/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.databinding_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var databinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
